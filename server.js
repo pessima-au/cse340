023c7b5b5ae49f2ea174cc25d3fa05284ef05235
pessima_au <albertpessima@outlook.com>
2025-11-13 19:35:41 +0000

    added inventory classification

--- server.js
+++ server.js
@@ -11,6 +11,7 @@ const env = require("dotenv").config();
 const app = express();
 const static = require("./routes/static");
 const baseController = require("./controllers/baseController");
+const inventoryRoute = require("./routes/inventoryRoute");
 
 /* ***********************
  * View Engine and Templates
@@ -24,11 +25,9 @@ app.set("layout", "./layouts/layout"); // not at views root
  *************************/
 app.use(static);
 
-// Index Route
-// app.get("/", function (req, res) {
-//   res.render("index", { title: "Home" });
-// });
+// Index Routes
 app.get("/", baseController.buildHome);
+app.use("/inv", inventoryRoute);
 
 /* ***********************
  * Local Server Information
